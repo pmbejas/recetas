12b5a3706c8c604a4d9580bd6b8378896903efb3
pmbejas <pmbejas@gmail.com>
2023-05-21 12:51:05 -0300

    Simple Menu Developed

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const handleClickMenu = (item) => {
+    const mainContent = document.getElementById('main-section');
+    switch (item) {
+        case 'inicio':
+            mainContent.innerHTML = itemInicio();
+            break;
+        case 'recetas':
+            mainContent.innerHTML = itemRecetas();
+            break;
+        case 'contacto':
+            mainContent.innerHTML = itemContacto();
+            break;
+    }
+}
+
+const itemInicio = () => {
+    const contenido = `<h1>INICIO</h1>`;
+    return contenido;
+}
+
+const itemRecetas = () => {
+    const contenido = `<h1>RECETAS</h1>`;
+    return contenido;
+}
+
+const itemContacto = () => {
+    const contenido = `<h1>CONTACTO</h1>`;
+    return contenido;
+}
